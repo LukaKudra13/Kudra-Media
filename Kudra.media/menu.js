cd628f2a5b85ddb00153fbce09f440b9bca11821
LukaKudra13 <89598900+LukaKudra13@users.noreply.github.com>
2022-12-03 15:58:35 +0100

    Add files via upload

---
+++ Kudra.media/menu.js
@@ -0,0 +1,15 @@
+document.getElementById("toggle").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("peek").classList.toggle("show");
+}
+
+document.getElementById("mail").onclick = function() {myFunction1()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction1() {
+  document.getElementById("peek2").classList.toggle("show");
+}
+
+
